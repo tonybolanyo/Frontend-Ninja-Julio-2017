f85236dade26951d12fc436631bc2dee7f3e0004
Alberto Casero <alberto@kasfactory.net>
2017-07-19 22:06:00 +0200

    Moficamos el gulpfile.js para generar todos los archivos en la carpeta dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,19 @@ var gulp = require("gulp"); // importamos la librería gulp
 var sass = require("gulp-sass");
 var notify = require("gulp-notify");
 var browserSync = require("browser-sync").create();
+var gulpImport = require("gulp-html-import");
 
 // definimos la tarea por defecto
-gulp.task("default", function(){
+gulp.task("default", ["html", "sass"], function(){
 
     // iniciamos el servidor de desarrollo
-    browserSync.init({ server: "src/" });
+    browserSync.init({ server: "dist/" });
 
     // observa cambios en los archivos SASS, y entonces ejecuta la tarea 'sass'
     gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]);
 
     // observa cambios en los archivos HTML y entonces recarga el navegador
-    gulp.watch("src/*.html", function(){
-        browserSync.reload();
-        notify().write("Navegador recargado");
-    });
+    gulp.watch("src/*.html", ["html"]);
 });
 
 // compilar sass
@@ -25,7 +23,16 @@ gulp.task("sass", function(){
         .pipe(sass().on("error", function(error){ // lo compilamos con gulp-sass
             return notify().write(error); // si ocurre un error, mostramos una notificación
         }))
-        .pipe(gulp.dest("src/css/")) // guardamos el resultado en la carpeta css
+        .pipe(gulp.dest("dist/")) // guardamos el resultado en la carpeta css
         .pipe(browserSync.stream()) // recargue el CSS del navegador
         .pipe(notify("SASS Compilado 🤘🏻")) // muestra notifiación en pantalla
+});
+
+// copiar e importar html
+gulp.task("html", function(){
+    gulp.src("src/*.html")
+        .pipe(gulpImport("src/components/"))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream())
+        .pipe(notify("HTML importado"));
 });
