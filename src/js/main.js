3a7ad758d79f1be7daa61ec8fe973babb7ae599d
Alberto Casero <alberto@kasfactory.net>
2017-07-24 22:33:16 +0200

    Cargamos las canciones desde el backend

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
-import sum from './example';
+window.$ = window.jQuery = require("jquery");
 
-console.log("SUMA", sum(5, 4));
+// Cargar la lista de canciones con AJAX
+$.ajax({
+    url: "/songs/",
+    success: songs => {
+        // Comprobamos si hay canciones
+        if (songs.length == 0) {
+            // Mostramos el estado vacío
+            $(".songs-list").removeClass("loading").addClass("empty");
+        } else {
+            // Componemos el HTML con todas las canciones
+            let html = "";
+            for (let song of songs) {
+                html += `<article class="song">
+                    <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
+                    <div class="artist">${song.artist}</div>
+                    <div class="title">${song.title}</div>
+                </article>`;
+            }
+
+            // Metemos el HTML en el div que contiene las canciones
+            $(".songs-list .ui-status.ideal").html(html);
+
+            // Quitamos el mensaje de cargando y mostramos la lista de canciones
+            $(".songs-list").removeClass("loading").addClass("ideal");
+        }
+    },
+    error: error => {
+        // Mostrar el estado de error
+        $(".songs-list").removeClass("loading").addClass("error");
+
+        // Hacemos log del error en la consola
+        console.error("Error al cargar las canciones", error);
+    }
+});
