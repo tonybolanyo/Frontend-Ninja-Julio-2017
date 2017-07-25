fdd243b73c29fcfb394f960ff5d1e930976a2457
Alberto Casero <alberto@kasfactory.net>
2017-07-26 00:21:46 +0200

    Implementamos la lógica de creación de una canción

---
+++ src/js/SongFormManager.js
@@ -0,0 +1,90 @@
+const $ = require("jquery");
+
+import UIManager from './UIManager';
+
+export default class SongFormManager extends UIManager {
+
+    constructor(elementSelector, songsService) {
+        super(elementSelector); // llamada al constructor de la clase UIManager
+        this.songsService = songsService;
+    }
+
+    init() {
+        this.setupSubmitEventHandler();
+    }
+
+    setupSubmitEventHandler() {
+        this.element.on("submit", () => {
+            this.validateAndSendData();
+            // en jQuery podemos hacer un preventDefault haciendo un return false en los manejadores de evento
+            return false; // == event.preventDefault();
+        });
+    }
+
+    validateAndSendData() {
+        if (this.isValid()) {
+            this.send();
+        }
+    }
+
+    isValid() {
+        const inputs = this.element.find("input");
+        for (let input of inputs) {
+            if (input.checkValidity() == false) {
+                const errorMessage = input.validationMessage;
+                input.focus();
+                this.setErrorHtml(errorMessage);
+                this.setError();
+                return false;
+            }
+        }
+        // Llegamos aquí, si no hay ningún error
+        this.setIdeal(); 
+        return true;
+    }
+
+    send() {
+        this.setLoading();
+        const song = {
+            artist: this.element.find("#artist").val(),
+            title: this.element.find("#title").val(),
+            cover_url: this.element.find("#cover_url").val()
+        };
+        this.songsService.save(song, success => {
+            // TODO: recargar el listado de canciones
+            this.resetForm();
+            this.setIdeal();
+        }, error => {
+            this.setErrorHtml("Se ha producido un error al guardar la canción en el servidor.");
+            this.setError();
+        });
+    }
+
+    resetForm() {
+        this.element[0].reset(); // resetea el formulario
+    }
+
+    disableFormControls() {
+        this.element.find("input, button").attr("disabled", true);
+    }
+
+    enableFormControls() {
+        this.element.find("input, button").attr("disabled", false);
+    }
+
+    setLoading() {
+        super.setLoading();
+        this.disableFormControls();
+    }
+
+    setError() {
+        super.setError();
+        this.enableFormControls();
+    }
+
+    setIdeal() {
+        super.setIdeal();
+        this.enableFormControls();
+    }
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,13 @@ window.$ = window.jQuery = require("jquery"); // Hace jQuery accesible públicam
 import SongsService from "./SongsService";
 import UIManager from "./UIManager";
 import SongsListManager from "./SongsListManager";
+import SongFormManager from "./SongFormManager";
 
 const songService = new SongsService("/songs/");
 const songsListUIManager = new UIManager(".songs-list");
 
 const songsListManager = new SongsListManager(songService, songsListUIManager);
 songsListManager.init();
+
+const songFormManager = new SongFormManager(".song-form", songService);
+songFormManager.init();
