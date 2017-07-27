4554288bbddb4ced856e3135b49169f6e7e6ee0b
Alberto Casero <alberto@kasfactory.net>
2017-07-28 00:33:31 +0200

    Implementamos el borrado de canciones refactorizando SongListManager para que  herede de UIManager (al igual que ocurre con SongFormManager)

--- src/js/SongsListManager.js
+++ src/js/SongsListManager.js
@@ -1,13 +1,20 @@
-export default class SongsListManager {
+import UIManager from './UIManager';
 
-    constructor(songsService, uiManager, pubSub) {
+export default class SongsListManager extends UIManager {
+
+    constructor(elementSelector, songsService, pubSub) {
+        super(elementSelector); // llamada al constructor de la clase UIManager
         this.songsService = songsService;
-        this.uiManager = uiManager;
         this.pubSub = pubSub;
     }
 
     init() {
         this.loadSongs();
+        let self = this;
+        this.element.on("click", ".song", function() {
+            let songId = this.dataset.id;
+            self.deleteSong(songId);
+        });
         this.pubSub.subscribe("new-song", (topic, song) => {
             this.loadSongs();
         });
@@ -18,16 +25,16 @@ export default class SongsListManager {
             // Comprobamos si hay canciones
             if (songs.length == 0) {
                 // Mostramos el estado vacío
-                this.uiManager.setEmpty();
+                this.setEmpty();
             } else {
                 // Componemos el HTML con todas las canciones
                 this.renderSongs(songs);
                 // Quitamos el mensaje de cargando y mostramos la lista de canciones
-                this.uiManager.setIdeal();
+                this.setIdeal();
             }
         }, error => {
             // Mostrar el estado de error
-            this.uiManager.setError();
+            this.setError();
             // Hacemos log del error en la consola
             console.error("Error al cargar las canciones", error);
         });
@@ -39,7 +46,7 @@ export default class SongsListManager {
             html += this.renderSong(song);
         }
         // Metemos el HTML en el div que contiene las canciones
-        this.uiManager.setIdealHtml(html);
+        this.setIdealHtml(html);
     }
 
     renderSong(song) {
@@ -49,11 +56,20 @@ export default class SongsListManager {
             cover_url = "img/disk-150px.png";
             srcset = ' srcset="img/disk-150px.png 150w, img/disk-250px.png 250w, img/disk-300px.png 300w"';
         }
-        return `<article class="song">
+        return `<article class="song" data-id="${song.id}">
                 <img src="${cover_url}" alt="${song.artist} - ${song.title}" class="cover"${srcset}>
                 <div class="artist">${song.artist}</div>
                 <div class="title">${song.title}</div>
             </article>`;
+    }
+
+    deleteSong(songId) {
+        this.setLoading();
+        this.songsService.delete(songId, success => {
+            this.loadSongs();
+        }, error => {
+            this.setError();
+        })
     }
 
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
 window.$ = window.jQuery = require("jquery"); // Hace jQuery accesible públicamente
 
 import SongsService from "./SongsService";
-import UIManager from "./UIManager";
 import SongsListManager from "./SongsListManager";
 import SongFormManager from "./SongFormManager";
 import PubSub from "pubsub-js";
 
 const songService = new SongsService("/songs/");
-const songsListUIManager = new UIManager(".songs-list");
 
-const songsListManager = new SongsListManager(songService, songsListUIManager, PubSub);
+const songsListManager = new SongsListManager(".songs-list", songService, PubSub);
 songsListManager.init();
 
 const songFormManager = new SongFormManager(".song-form", songService, PubSub);
