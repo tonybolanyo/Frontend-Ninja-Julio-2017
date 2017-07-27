6360a1fcdc8f4e8f02488c4ebd7a195219f1eedf
Alberto Casero <alberto@kasfactory.net>
2017-07-28 00:15:01 +0200

    Modificamos el renderizado de una canción para mostrar un placeholder en caso de que la canción no tenga URL del cover

--- src/js/SongsListManager.js
+++ src/js/SongsListManager.js
@@ -43,8 +43,14 @@ export default class SongsListManager {
     }
 
     renderSong(song) {
+        let cover_url = song.cover_url;
+        let srcset = "";
+        if (cover_url == "") {
+            cover_url = "img/disk-150px.png";
+            srcset = ' srcset="img/disk-150px.png 150w, img/disk-250px.png 250w, img/disk-300px.png 300w"';
+        }
         return `<article class="song">
-                <img src="${song.cover_url}" alt="${song.artist} - ${song.title}" class="cover">
+                <img src="${cover_url}" alt="${song.artist} - ${song.title}" class="cover"${srcset}>
                 <div class="artist">${song.artist}</div>
                 <div class="title">${song.title}</div>
             </article>`;
